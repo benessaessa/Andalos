7dd96bc93e60bb449e0c7815c605c22de7606beb
mohamed <moh.essa2020@gmail.com>
2020-11-24 19:02:42 +0200

    slider

--- js/file.js
+++ js/file.js
@@ -36,3 +36,41 @@ breakpoints: {
     }
 }
 });
+var swiper = new Swiper('.swiper-container2', {
+  // slidesPerView: 3,
+// spaceBetween: 15,
+freeMode: true,
+slidesPerView: 'auto',
+pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+},
+autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+breakpoints: {
+    // when window width is <= 499px
+    320: {
+        slidesPerView: 1,
+        spaceBetweenSlides: 10
+    },
+    700:{
+        slidesPerView: 2,
+        spaceBetweenSlides: 50
+    },
+    // when window width is <= 999px
+    999: {
+        slidesPerView: 2,
+        spaceBetweenSlides: 15
+    },
+    1200: {
+        slidesPerView: 3,
+        spaceBetweenSlides: 15
+    }
+}
+});
