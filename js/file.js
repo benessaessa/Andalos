154487262f58d24604feaa2ebe86a329b75bff7a
mohamed <moh.essa2020@gmail.com>
2020-11-24 15:25:57 +0200

    swiper all pages

---
+++ js/file.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper('.swiper-container', {
+  // slidesPerView: 3,
+// spaceBetween: 15,
+freeMode: true,
+slidesPerView: 'auto',
+pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+},
+autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+breakpoints: {
+    // when window width is <= 499px
+    320: {
+        slidesPerView: 1,
+        spaceBetweenSlides: 20
+    },
+    700:{
+        slidesPerView: 3,
+        spaceBetweenSlides: 50
+    },
+    // when window width is <= 999px
+    999: {
+        slidesPerView: 4,
+        spaceBetweenSlides: 15
+    },
+    1200: {
+        slidesPerView: 7,
+        spaceBetweenSlides: 15
+    }
+}
+});
