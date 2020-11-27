3767d2bb056c9e6a9ee74a2ffd1d52688ed3ceaf
mohamed <moh.essa2020@gmail.com>
2020-11-27 11:20:31 +0200

    edits ramy

--- js/file.js
+++ js/file.js
@@ -21,6 +21,10 @@ breakpoints: {
         slidesPerView: 1,
         spaceBetweenSlides: 20
     },
+    500: {
+        slidesPerView: 2,
+        spaceBetweenSlides: 20
+    },
     700:{
         slidesPerView: 3,
         spaceBetweenSlides: 50
@@ -74,3 +78,39 @@ breakpoints: {
     }
 }
 });
+$('.navbar').on('shown.bs.collapse', function () {
+    $('.newSocial2').css({'margin-right':'auto','margin-left':'unset'});
+    $('.newSocial').css('margin-left','2rem');
+});
+$('.navbar').on('hidden.bs.collapse', function () {
+    $('.newSocial').css({'margin-left':'auto','margin-right':'unset'});
+    $('.newSocial2').css({'margin-left':'auto','margin-right':'1rem'});
+});
+$(window).scroll(testScroll);
+    var viewed = false;
+
+    function isScrolledIntoView(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+    function testScroll() {
+    if (isScrolledIntoView($(".count")) && !viewed) {
+        viewed = true;
+        $('.count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+    }
+}
